a2876e835bda97b4cfd547a90e46738cbec07802
EuricoSerradasDuarte <51817014+EuricoSerradasDuarte@users.noreply.github.com>
2019-07-09 15:55:46 +0100

    Delete Eurico_7.py

--- Eurico_7.py
+++
@@ -1,7 +0,0 @@
-escolha = int(input("Insira um número: "))
-resultado = escolha
-repeticoes = 1
-while escolha>repeticoes + 1:
-    resultado = resultado*(escolha-repeticoes)
-    repeticoes = repeticoes + 1
-print(resultado)
